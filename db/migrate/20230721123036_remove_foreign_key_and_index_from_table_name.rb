748476f43ca3be9b6699938f96214213fc5830bc
kayemba <kayembejonathan5@gmail.com>
2023-07-21 19:26:45 +0200

    Add update the App

--- db/migrate/20230721123036_remove_foreign_key_and_index_from_table_name.rb
+++
@@ -1,7 +0,0 @@
-class RemoveForeignKeyAndIndexFromTableName < ActiveRecord::Migration[7.0]
-  def change
-    remove_foreign_key :child_table, :parent_table
-    remove_index :child_table, :parent_id
-    remove_column :child_table, :parent_id
-  end
-end
--- db/migrate/20230721124151_change_user_ref_to_likes.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUserRefToLikes < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :likes, :author, foreign_key: { to_table: 'users'}
-  end
-end
