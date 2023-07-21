6d9a620e3615ab719557e61b20c27bcdda47ced6
kayemba <kayembejonathan5@gmail.com>
2023-07-21 12:19:07 +0200

    Add update and fix the app

--- db/migrate/20230721090102_create_users.rb
+++ db/migrate/20230721090102_create_users.rb
@@ -1,14 +1,12 @@
-# frozen_string_literal: true
-
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.text :photo
-      t.text :bio
+      t.string :photo
+      t.string :bio
+      t.integer :postsCounter
 
       t.timestamps
     end
-    add_index :users, 
   end
 end
