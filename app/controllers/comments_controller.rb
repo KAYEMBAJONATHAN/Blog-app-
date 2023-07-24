8d2c11700ffbfee698408daefc17fd8bef55038d
kayemba <kayembejonathan5@gmail.com>
2023-07-24 20:03:10 +0200

    Add controllers specs

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,19 @@
-class CommentsController < ApplicationController
-  def index
-    # Placeholder for displaying a list of users
+class LikesController < ActionController::Base
+  def create
+    @like = Like.new(like_params)
+    @like.save
+    redirect_to likes_path
   end
 
-  def show
-    # Placeholder for displaying a single user
+  def destroy
+    @like = Like.find(params[:id])
+    @like.destroy
+    redirect_to likes_path
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:author_id, :post_id)
   end
 end
