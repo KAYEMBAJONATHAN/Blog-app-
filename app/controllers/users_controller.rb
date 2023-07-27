64cd93ec7a5e9f48c0ced8abd62a99dd8ff64fd7
kayemba <kayembejonathan5@gmail.com>
2023-07-27 19:26:02 +0200

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @recent_posts = @user.posts.order(created_at: :desc).limit(5)
+    @post = @user.posts.order(created_at: :desc).limit(5)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'users#index'
   get '/users/index', to: 'users#index', as: 'users_index'
   resources :users, only: [:index, :show] do
   resources :posts, only: [:index, :show]
